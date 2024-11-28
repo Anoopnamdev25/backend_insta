7060cf9bc53159d7d5cbc068c4b914a0414bb0da
anoopnamdev25 <namdevanoop8517@gmail.com>
2024-11-28 12:17:26 +0530

    CORS

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'http://localhost:3000', // Allow requests from your React app
+      }
+));
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve uploaded images
 
